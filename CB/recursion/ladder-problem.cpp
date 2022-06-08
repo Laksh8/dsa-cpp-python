2dce258e767d917ba0658a2bd40696058b0ea811
Lakshit Khanna <lakshitkhanna311@gmail.com>
2022-06-08 12:00:05 +0530

    contest 8 in progress

--- CB/recursion/ladder-problem.cpp
+++ CB/recursion/ladder-problem.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
+
+int countWays(int n,int k){
+    // base case
+    if(n==0){
+        return 1;
+    }
+
+    // recursion 
+    int count = 0;
+    for(int j=1;j<=k;j++){
+        if(n-j>=0){
+            count+= countWays(n-j,k);
+        }
+    }
+    return count;
+}
+
+
 int main(){
     int n,k;
     cin>>n>>k;
-    
+    cout<<countWays(n,k)<<endl;
     return 0;
 }
